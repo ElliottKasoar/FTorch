e4a76e08316e31e8a8cd82e3e60eec7b5d1ef664
Jack Atkinson <jwa34@login-e-12.data.cluster>
2022-10-28 11:47:27 +0100

    Resolving fortran indexing issues

--- fortran-pytorch-lib/MNIST_inference.f90
+++ fortran-pytorch-lib/MNIST_inference.f90
@@ -15,9 +15,9 @@ program inference
    real(c_float), dimension(:,:), allocatable, target :: out_data
 
    integer(c_int), parameter :: in_dims = 3
-   integer(c_int64_t) :: in_shape(in_dims) = [1, 28, 28]
+   integer(c_int64_t) :: in_shape(in_dims) = [10, 28, 28]
    integer(c_int), parameter :: out_dims = 2
-   integer(c_int64_t) :: out_shape(out_dims) = [1, 10]
+   integer(c_int64_t) :: out_shape(out_dims) = [10, 10]
 
    character(len=:), allocatable :: filename
 
@@ -25,16 +25,16 @@ program inference
    real :: max_el
 
    ! Allocate one-dimensional input/output arrays, based on multiplication of all input/output dimension sizes
-   allocate(in_data(in_shape(1), in_shape(2), in_shape(3)))
-   allocate(out_data(out_shape(1), out_shape(2)))
+   allocate(in_data(in_shape(3), in_shape(2), in_shape(1)))
+   allocate(out_data(out_shape(2), out_shape(1)))
 
    ! Initialise data
    ! read data from file:
    open(unit=18, file='test_data.txt' , status='old', &
         access ='sequential',form='formatted')
-   do i = 1, 1
+   do i = 1, 10
       do j = 1, 28
-         read(18,*)(in_data(i,k,j),k=1,28)
+         read(18,*)(in_data(j,k,i),k=1,28)
       enddo
    enddo
    close(18)
@@ -51,7 +51,7 @@ program inference
    ! write (*,*) out_data(1, 1000)
 
    write(*,*) 'TorchScript model predictions'
-   write(*,*) maxloc(out_data, dim = 2)
+   write(*,*) maxloc(out_data, dim = 1)
    ! write(*,*) out_data
 
    ! Cleanup
